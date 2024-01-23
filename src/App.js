d789e19763b440c76b5b17de5d83e9d7335cceca
Sanchit Lumber <sanchit.sah@lumberfi.com>
2024-01-23 11:41:19 +0530

    Add @sentry/react package and initialize Sentry for error tracking and performance monitoring

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const methodDoesNotExist = ()=>{
+    throw new Error('There is an error!');
+  } 
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <button onClick={() => methodDoesNotExist()}>Break the world</button>
       </header>
     </div>
   );
